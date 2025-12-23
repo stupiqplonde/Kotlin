6453af0f35f8ba12b1e4bb050c66f97c74dbfa6d
stupiqplonde <kovalevmihail38@gmail.com>
2025-12-23 13:49:20 +0300

    Update GameCharacter.kt

--- src/main/kotlin/lesson10/GameCharacter.kt
+++ src/main/kotlin/lesson10/GameCharacter.kt
@@ -11,8 +11,9 @@ class GameCharacter(
     val baseAttack: Int
 ) {
     var currentHealth: Int = maxHealth
-
+    var attackBonus: Int = 0
     var currentMana: Int = maxMana
+    var damage = baseAttack + attackBonus
 
     var canAttack: Boolean = true
     // флаг - точка опоры проверки состояния
@@ -23,6 +24,9 @@ class GameCharacter(
     // ? - возможность вернуть null
 
     private var potionJob: Job? = null
+    private var attackBuffJob: Job? = null
+
+    fun isAlive() = currentHealth > 0
 
     fun printStatus() {
         println("статус персонажа $name ")
@@ -37,7 +41,7 @@ class GameCharacter(
             return
         }
 
-        if (currentHealth <= 0) {
+        if (!isAlive()) {
             println("[!} $name sdox")
             return
         }
@@ -52,16 +56,20 @@ class GameCharacter(
     }
 
     fun takeDamage(amount: Int) {
-        if (amount <= 0) {
-            println("$name не получает урина")
-            return
-        }
+        if (isAlive()) {
+            if (amount <= 0) {
+                println("$name не получает урина")
+                return
+            }
 
-        currentHealth -= amount
-        println("[-] $name получает $amount урина | HP: $currentHealth")
+            currentHealth -= amount
+            println("[-] $name получает $amount урина | HP: $currentHealth")
 
-        if (currentHealth <= 0) {
-            println("[!] $name повержен!")
+            if (currentHealth <= 0) {
+                println("[!] $name повержен!")
+            }
+        }else{
+            println("$name died")
         }
     }
 
@@ -89,36 +97,40 @@ class GameCharacter(
         amuotPerTick: Int,
         intervalMillis: Long
     ) {
-        if (manaRegenJob != null) {
-            println("[!] Мана уже регенится")
-            manaRegenJob?.cancel()
-        }
-
-        manaRegenJob = GlobalScope.launch {
-            println("Реген маны для $name запущена")
-
-            while (true) {
-                delay(intervalMillis)
-
-                if (currentHealth <= 0) {
-                    println("[!] $name повержен, реген прерван")
-                    break
-                }
-
-                if (currentMana >= maxMana) {
-                    println("[!] мана $name уже полная")
-                    continue
-                }
-
-                currentMana += amuotPerTick
-
-                if (currentMana > maxMana) {
-                    currentMana = maxMana
-                }
-
-                println("[+] $name регенит $amuotPerTick маны ($currentMana / $maxMana")
+        if (isAlive()) {
+            if (manaRegenJob != null) {
+                println("[!] Мана уже регенится")
+                manaRegenJob?.cancel()
             }
-            println("Корутина регена маны для $name завершена")
+
+            manaRegenJob = GlobalScope.launch {
+                println("Реген маны для $name запущена")
+
+                while (true) {
+                    delay(intervalMillis)
+
+                    if (currentHealth <= 0) {
+                        println("[!] $name повержен, реген прерван")
+                        break
+                    }
+
+                    if (currentMana >= maxMana) {
+                        println("[!] мана $name уже полная")
+                        continue
+                    }
+
+                    currentMana += amuotPerTick
+
+                    if (currentMana > maxMana) {
+                        currentMana = maxMana
+                    }
+
+                    println("[+] $name регенит $amuotPerTick маны ($currentMana / $maxMana")
+                }
+                println("Корутина регена маны для $name завершена")
+            }
+        }else{
+            println("$name died")
         }
     }
 
@@ -140,34 +152,38 @@ class GameCharacter(
         ticks: Int,
         intervalMillis: Long
     ){
-        if (potionJob != null){
-            println("яд действует на $name эффект перезапущен")
-            potionJob?.cancel()
-        }
-
-        potionJob = GlobalScope.launch {
-            println("[-] $name отравлен, яд действует $ticks тиков")
-
-            var remaningTicks = ticks
-
-            while (remaningTicks > 0){
-                delay(intervalMillis)
-                // ждем интервал между тиками
-
-                if (currentHealth <= 0){
-                    println("$name погиб, яд не действует")
-                    break
-                }
-
-                println("яд наносить $damagePerTick урина $name")
-                takeDamage(damagePerTick)
-                // получение урина от яда за 1 тик
-
-                remaningTicks -= 1
-                // после получения отнимаем один тик от оставшегося времени наложения
-
+        if (isAlive()) {
+            if (potionJob != null) {
+                println("яд действует на $name эффект перезапущен")
+                potionJob?.cancel()
             }
-            println("[!] Эффект яда на $name завершился")
+
+            potionJob = GlobalScope.launch {
+                println("[-] $name отравлен, яд действует $ticks тиков")
+
+                var remaningTicks = ticks
+
+                while (remaningTicks > 0) {
+                    delay(intervalMillis)
+                    // ждем интервал между тиками
+
+                    if (currentHealth <= 0) {
+                        println("$name погиб, яд не действует")
+                        break
+                    }
+
+                    println("яд наносить $damagePerTick урина $name")
+                    takeDamage(damagePerTick)
+                    // получение урина от яда за 1 тик
+
+                    remaningTicks -= 1
+                    // после получения отнимаем один тик от оставшегося времени наложения
+
+                }
+                println("[!] Эффект яда на $name завершился")
+            }
+        }else{
+            println("$name died")
         }
     }
 
@@ -181,5 +197,52 @@ class GameCharacter(
         potionJob?.cancel()
         println("[!] с $name снять эффект")
         potionJob = null
+    }
+
+    fun applyAttackBuff(
+        bonus: Int,
+        ticks: Int,
+        durationMillis: Long
+    ) {
+        if (isAlive()) {
+            if (attackBuffJob != null) {
+                println("бафф действует на $name, эффект перезапущен")
+                attackBuffJob?.cancel()
+            }
+
+            attackBuffJob = GlobalScope.launch {
+                println("у $name бафф силы, эффект действует $durationMillis")
+
+                var remaningTicks = ticks
+
+                while (remaningTicks > 0) {
+                    delay(durationMillis)
+                    // ждем интервал между тиками
+
+                    println("бафф добавляет к урону $bonus")
+                    attackBonus += bonus
+                    // получение баффа за тик
+
+                    remaningTicks -= 1
+                    // после получения отнимаем один тик от оставшегося времени наложения
+
+                }
+                println("[!] Эффект баффа на $name завершился")
+            }
+        }else{
+            println("$name died")
+        }
+    }
+
+    fun clearAttackBuff(){
+        // принудительное снятие эффекта
+        if (attackBuffJob == null){
+            println("на $name нет баффов")
+            return
+        }
+
+        attackBuffJob?.cancel()
+        println("[!] с $name снять бафф")
+        attackBuffJob = null
     }
 }
