7e8a545a7cf8e823b4b234387f4710dd9d978c01
stupiqplonde <kovalevmihail38@gmail.com>
2026-02-19 14:00:53 +0300

    Create questJournal.kt

---
+++ kotlin/Kool-e/lesson6/questJournal.kt
@@ -0,0 +1,828 @@
+package lesson6
+
+import de.fabmax.kool.KoolApplication           // KoolApplication - запускает Kool-приложение (окно + цикл рендера)
+import de.fabmax.kool.addScene                  // addScene - функция "добавь сцену" в приложение (у тебя она просила отдельный импорт)
+import de.fabmax.kool.math.FLT_EPSILON
+
+import de.fabmax.kool.math.Vec3f                // Vec3f - 3D-вектор (x, y, z), как координаты / направление
+import de.fabmax.kool.math.deg                  // deg - превращает число в "градусы" (угол)
+import de.fabmax.kool.scene.*                   // scene.* - Scene, defaultOrbitCamera, addColorMesh, lighting и т.д.
+
+import de.fabmax.kool.modules.ksl.KslPbrShader  // KslPbrShader - готовый PBR-шейдер (материал)
+import de.fabmax.kool.util.Color                // Color - цвет (RGBA)
+import de.fabmax.kool.util.Time                 // Time.deltaT - сколько секунд прошло между кадрами
+
+import de.fabmax.kool.pipeline.ClearColorLoad   // ClearColorLoad - режим: "не очищай экран, оставь то что уже нарисовано"
+
+import de.fabmax.kool.modules.ui2.*             // UI2: addPanelSurface, Column, Row, Button, Text, dp, remember, mutableStateOf
+import jdk.jshell.execution.JdiInitiator
+import kotlinx.coroutines.flow.StateFlow
+import lesson2.gameState
+import lesson3.DamageDealt
+import lesson3.ItemAdded
+import lesson3.SWORD
+import lesson5.QuestState
+import java.awt.Choice
+import java.awt.dnd.DropTarget
+import java.io.File
+import java.security.Guard
+import javax.xml.stream.events.StartElement
+
+// startWith('quest:') - проверка с чего начинается строка
+// substringAfter('quest:') - добавить кусок строки после префикса
+// try {что пытаемся сделать} catch (e: Exception) {сделать то, что произойдет в случае падения при загрузке try}
+// try catch - не положит весь код fun main, если произойдет ошибка
+
+class GameState{
+    val playerId = mutableStateOf("Oleg")
+
+    val hp = mutableStateOf(100)
+    val gold = mutableStateOf(0)
+
+    val log = mutableStateOf<List<String>>(emptyList())
+}
+
+fun pushLog(game: GameState, text: String){
+    game.log.value = (game.log.value + text).takeLast(20)
+}
+
+// sealed - иерархия
+// это вид классов который только хранит в себе другие классы
+// interface - тип классов который обязует все дочерние классы перезаписать свойства которые мы положим во вторичный конструктор
+sealed interface GameEvent{
+    val playerId: String
+}
+
+data class TalkToNpc(
+    override val playerId: String,
+    val npcId: String
+): GameEvent
+
+data class ChoiceSelected(
+    override val playerId: String,
+    val npcId: String,
+    val choiceId: String
+): GameEvent
+
+data class ItemCollected(
+    override val playerId: String,
+    val itemId: String,
+    val count: Int
+): GameEvent
+
+data class ItemGivenToNpc(
+    override val playerId: String,
+    val itemId: String,
+    val npcId: String,
+    val count: Int
+): GameEvent
+
+data class GoldPaidToNpc(
+    override val playerId: String,
+    val npcId: String,
+    val count: Int
+): GameEvent
+
+data class QuestStateChanged(
+    override val playerId: String,
+    val questId: String,
+    val newStateName: String
+): GameEvent
+
+data class PlayerProgressSaved(
+    override val playerId: String,
+    val reason: String
+): GameEvent
+
+
+// EventBus
+
+typealias Listener = (GameEvent) -> Unit
+
+class EventBus{
+    private val listeners = mutableListOf<Listener>()
+
+    fun subscribe(listener: Listener){
+        listeners.add(listener)
+    }
+
+    fun publish(event: GameEvent){
+        for (listener in listeners){
+            listener(event)
+        }
+    }
+}
+
+// dialogue System
+
+data class DialogueOption(
+    val id: String,
+    val text: String
+)
+
+data class DialogueView(
+    val npcName: String,
+    val text: String,
+    val options: List<DialogueOption>
+)
+
+// QuestDefinition - описание квеста, будет интерфейсом, то есть набор правил для всех квестов при их создании
+// Любой новый квест при создании будет наследовать из данного интерфейса все свойства методы
+
+interface QuestDefinition{
+    val questId: String
+
+    fun initialStateName(): String
+    // состояние, которое будет принимать квест в момент создания
+
+    fun nextStateName(currentStateName: String, event: GameEvent, game: GameState): String
+    // метод, который проверяет нынешнее состояние и возвращает следующее к которому он перейдет при event событии
+
+    fun stateDescription(stateName: String): String
+    // описание этапа квеста для квестового журнала
+
+    fun npcDialogue(stateName: String): DialogueView
+    // метод указывает что скажет npc и какие кнопки покажет в диалоге
+}
+
+// создание квеста с алхимиком (экземпляр интерфейса QuestDefinition)
+
+enum class AlchemistState{
+    START,
+    OFFERED,
+    HELP_ACCEPTED,
+    HERB_COLLECTED,
+    THREAT_ACCEPTED,
+    GOOD_END,
+    EVIL_END
+}
+
+class AlchemistQuest: QuestDefinition{
+    override val questId: String = "q_alchemist"
+
+    override fun initialStateName(): String {
+        return AlchemistState.START.name
+    }
+
+    private fun safeState(stateName: String): AlchemistState{
+        // valueOf - может сломать наш код есл истрока окажется неправильной
+        return try {
+            AlchemistState.valueOf(stateName)
+        } catch (e: Exception){
+            AlchemistState.START
+        }
+    }
+
+    override fun nextStateName(currentStateName: String, event: GameEvent, game: GameState): String {
+        val current = safeState(currentStateName)
+
+        val next: AlchemistState = when(current){
+            AlchemistState.START -> when(event){
+                is TalkToNpc -> {
+                    if (event.npcId == "Alchemist") AlchemistState.OFFERED else AlchemistState.START
+                }
+                else -> AlchemistState.START
+            }
+
+            AlchemistState.OFFERED -> when(event){
+                is ChoiceSelected -> {
+                    if (event.npcId != "Alchemist") AlchemistState.OFFERED
+                    else if (event.choiceId == "help") AlchemistState.HELP_ACCEPTED
+                    else if (event.choiceId == "thret") AlchemistState.THREAT_ACCEPTED
+                    else AlchemistState.OFFERED
+                }
+                else -> AlchemistState.OFFERED
+            }
+
+            AlchemistState.HELP_ACCEPTED -> when(event){
+                is ItemCollected -> {
+                    if (event.itemId == "herb") AlchemistState.HELP_ACCEPTED else AlchemistState.HELP_ACCEPTED
+                }
+                else -> AlchemistState.OFFERED
+            }
+
+            AlchemistState.HERB_COLLECTED -> when(event){
+                is ItemGivenToNpc -> {
+                    if (event.npcId == "Alchemist" && event.itemId == "herb") AlchemistState.GOOD_END
+                    else AlchemistState.HERB_COLLECTED
+                }
+                else -> AlchemistState.HERB_COLLECTED
+            }
+
+            AlchemistState.THREAT_ACCEPTED -> when(event){
+                is ChoiceSelected -> {
+                    if (event.npcId == "Alchemist" && event.choiceId == "threat_confirm") AlchemistState.EVIL_END
+                    else AlchemistState.THREAT_ACCEPTED
+                }
+                else -> AlchemistState.THREAT_ACCEPTED
+            }
+
+            AlchemistState.GOOD_END -> AlchemistState.GOOD_END
+            AlchemistState.EVIL_END -> AlchemistState.EVIL_END
+        }
+        return next.name
+    }
+
+    override fun stateDescription(stateName: String): String {
+        return when(safeState(stateName)){
+            AlchemistState.START -> "Поговорить с Алхимиком"
+            AlchemistState.OFFERED -> "Помочь или угрожать"
+            AlchemistState.HELP_ACCEPTED -> "Собрать 1 траву"
+            AlchemistState.HERB_COLLECTED -> "Отдать траву алпедику"
+            AlchemistState.THREAT_ACCEPTED -> "Подтвердить угрозу"
+            AlchemistState.GOOD_END -> "Квест завершен (хорошо)"
+            AlchemistState.EVIL_END -> "Квест завершен (плоха)"
+        }
+    }
+
+    override fun npcDialogue(stateName: String): DialogueView {
+        return when(safeState(stateName)){
+            AlchemistState.START -> DialogueView(
+                "Алпедик",
+                "Привет кому? ЛИЗЕ!? лизе респект (пиздуй за травой)",
+                listOf(DialogueOption("talk", "Поговорить"))
+            )
+            AlchemistState.OFFERED -> DialogueView(
+                "Алпедик",
+                "Привет кому? ЛИЗЕ!? лизе респект (пиздуй за травой)",
+                listOf(
+                    DialogueOption("help", "Помочь (принести траву)"),
+                    DialogueOption("threat", "Угрожать (принести траву)"),
+                )
+            )
+
+            AlchemistState.HELP_ACCEPTED -> DialogueView(
+                "Алпедик",
+                "принеси травы курнуть",
+                listOf(
+                    DialogueOption("collect_herb", "Cобрать траву"),
+                    DialogueOption("talk", "Говорить еще"),
+                )
+            )
+
+            AlchemistState.HERB_COLLECTED -> DialogueView(
+                "Алпедик",
+                "Дай сюда",
+                listOf(
+                    DialogueOption("give_herb", "отдать одну траву"),
+                )
+            )
+
+            AlchemistState.THREAT_ACCEPTED -> DialogueView(
+                "Алпедик",
+                "Ты уверенв.?",
+                listOf(
+                    DialogueOption("thereat_confirm", "гони косарь"),
+                )
+            )
+
+            AlchemistState.GOOD_END -> DialogueView(
+                "Алхимик",
+                "Спасибо, держи зелье здоровья (GOOD END)",
+                emptyList()
+            )
+
+            AlchemistState.EVIL_END -> DialogueView(
+                "Алхимик",
+                "Ghjdfkbdfq jnc.lf! tckb ;bpym ljhjuf! (EVIL END)",
+                emptyList()
+            )
+        }
+    }
+}
+
+// квест со стражником (вещь или бан)
+
+enum class GuardState{
+    START,
+    OFFERED,
+    WAIT_PAYMENT,
+    PASSED,
+    BANNED
+}
+
+class GuardQuest: QuestDefinition{
+    override val questId: String = "q_guard"
+
+    override fun initialStateName(): String = GuardState.START.name
+
+    private fun safeState(stateName: String): GuardState{
+        // valueOf - может сломать наш код есл истрока окажется неправильной
+        return try {
+            GuardState.valueOf(stateName)
+        } catch (e: Exception){
+            GuardState.START
+        }
+    }
+
+    override fun nextStateName(currentStateName: String, event: GameEvent, game: GameState): String {
+        val current = safeState(currentStateName)
+
+        val next: GuardState = when(current){
+            GuardState.START -> when(event){
+                is TalkToNpc -> {
+                    if (event.npcId == "guard") GuardState.OFFERED else GuardState.START
+                }
+                else -> GuardState.START
+            }
+
+            GuardState.OFFERED -> when (event){
+                is ChoiceSelected -> {
+                    if (event.npcId != "guard") GuardState.OFFERED
+                    else if (event.choiceId == "pay") GuardState.WAIT_PAYMENT
+                    else if (event.choiceId == "refuse") GuardState.BANNED
+                    else GuardState.OFFERED
+                }
+                else -> GuardState.OFFERED
+            }
+
+            GuardState.WAIT_PAYMENT -> when(event){
+                is GoldPaidToNpc -> {
+                    if (event.npcId == "guard" && event.count >= 5) GuardState.PASSED
+                    else GuardState.WAIT_PAYMENT
+                }
+                else -> GuardState.WAIT_PAYMENT
+            }
+
+            GuardState.PASSED -> GuardState.PASSED
+            GuardState.BANNED -> GuardState.BANNED
+        }
+        return next.name
+    }
+
+    override fun stateDescription(stateName: String): String {
+        return when(safeState(stateName)){
+            GuardState.START -> "Подойти к Стражнику"
+            GuardState.OFFERED -> "Надо заплатить!"
+            GuardState.WAIT_PAYMENT -> "Заплатить за прохождение"
+            GuardState.PASSED -> "Пройти"
+            GuardState.BANNED -> "пока пока"
+        }
+    }
+
+    override fun npcDialogue(stateName: String): DialogueView {
+        return when(safeState(stateName)){
+            GuardState.START -> DialogueView(
+                "Страж",
+                "Привет, ты куда?",
+                listOf(DialogueOption("talk", "Поговорить"))
+            )
+            GuardState.OFFERED -> DialogueView(
+                "Страж",
+                "Для прохода - плати!",
+                listOf(
+                    DialogueOption("talk", "Говорить еще")
+                )
+            )
+
+            GuardState.WAIT_PAYMENT -> DialogueView(
+                "Страж",
+                "Плати быстрее!",
+                listOf(
+                    DialogueOption("pay", "оплатить"),
+                    DialogueOption("refuse", "отказаться"),
+                )
+            )
+
+            GuardState.PASSED -> DialogueView(
+                "Страж",
+                "Тебе повезло, шерсть",
+                listOf(
+                    DialogueOption("give_herb", "отдать одну траву"),
+                )
+            )
+
+            GuardState.BANNED -> DialogueView(
+                "Страж",
+                "иди нахрюк",
+                listOf(
+                    DialogueOption("thereat_confirm", "гони косарь"),
+                )
+            )
+        }
+    }
+}
+
+class QuestManager(
+    private val bus: EventBus,
+    private val game: GameState,
+    private val quests: List<QuestDefinition>
+){
+    val stateByPlayer = mutableStateOf<Map<String, Map<String, String>>>(emptyMap())
+    // внешний ключ - playerId
+    // внутренний ключ - questId
+    // внутреннее значение - состояние квеста на момент сохранения
+
+    init {
+        bus.subscribe { event ->
+            handleEvent(event)
+        }
+    }
+
+    fun getStateName(playerId: String, questId: String): String {
+        val playerMap = stateByPlayer.value[playerId]
+
+        if (playerMap == null) {
+            val def = quests.firstOrNull{it.questId == questId}
+            return def?.initialStateName() ?: "UNKNOWN"
+        }
+        return playerMap[questId] ?: (quests.firstOrNull{it.questId == questId}?.initialStateName() ?: "UNKNOWN")
+    }
+
+    fun setStateName(playerId: String, questId: String, stateName: String){
+        val outerCopy = stateByPlayer.value.toMutableMap()
+
+        val innerOld = outerCopy[playerId] ?: emptyMap()
+
+        val innerCopy = innerOld.toMutableMap()
+        innerCopy[playerId] = stateName
+
+        outerCopy[playerId] = innerCopy.toMap()
+        stateByPlayer.value = outerCopy.toMap()
+    }
+
+    private fun handleEvent(event: GameEvent){
+        val player = event.playerId
+
+        for (quest in quests){
+            val currentState = stateByPlayer
+            val nextState = currentState.nextState
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
